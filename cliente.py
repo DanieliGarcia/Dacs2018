81079e549e9ed5bc12eb6c0829fbbc820a8ed728
DanieliGarcia <danieli.garcia@univille.br>
2018-10-24 20:09:19 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,9 @@
+class Client():
+    def __init__(self, id=0, name=None,\
+                 phone=None,email=None):
+        self._= id
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+
